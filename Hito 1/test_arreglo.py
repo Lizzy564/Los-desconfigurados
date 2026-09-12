107fd3262d66006c88b7c98e219cb41fa3cc280e
Lorenita_07 <medina.buitrago209@gmail.com>
2026-09-12 12:38:25 -0500

    Update test_arreglo.py

--- Hito 1/test_arreglo.py
+++ Hito 1/test_arreglo.py
@@ -2,9 +2,9 @@ from arreglo_dinamico import ArregloDinamico
 from paciente import Paciente
 
 arreglo = ArregloDinamico(2)
-paciente = Paciente(1116182650, 20, 2, "08:30")
-paciente2 = Paciente(1202566655, 25, 3, "04:50")
-paciente3 = Paciente(1505454555, 12, 2, "01:12")
+paciente = Paciente(1116182650, 20, 2, 30)
+paciente2 = Paciente(1202566655, 25, 3, 35)
+paciente3 = Paciente(1505454555, 12, 2, 42)
 
 arreglo.insertar(paciente)
 arreglo.insertar(paciente2)
@@ -20,3 +20,8 @@ arreglo.eliminar(1202566655)
 print("La cantidad después de eliminar es: ", arreglo.longitud())
 print("El ID del primer paciente es: ", arreglo.obtener(0).id)
 print("El ID del segundo paciente es: ", arreglo.obtener(1).id)
+
+
+arreglo_poisson = ArregloDinamico()
+arreglo_poisson.generar_llegadas_poisson(5)
+print("Los pacientes generados con Poisson: ", arreglo_poisson.longitud())
