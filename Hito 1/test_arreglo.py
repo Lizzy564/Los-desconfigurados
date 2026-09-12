4aaee554745994616694e903e4eaa3d7c0cb7a6e
Lorenita_07 <medina.buitrago209@gmail.com>
2026-09-11 22:36:46 -0500

    Update test_arreglo.py

--- Hito 1/test_arreglo.py
+++ Hito 1/test_arreglo.py
@@ -1,1 +1,9 @@
+from paciente import Paciente 
+from arreglo_dinamico import ArregloDinamico 
 
+arreglo = ArregloDinamico()
+paciente = Paciente(1, 20, 2, "08:30")
+arreglo.insertar(paciente)
+
+print("La cantidad de pacientes es:", arreglo.longitud())
+print("El ID del paciente es:", arreglo.obtener(0).id)
