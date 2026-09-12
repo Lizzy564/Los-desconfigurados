472d5c291a1fa78732c4ab0a9d2ba26ba38b0090
Lorenita_07 <medina.buitrago209@gmail.com>
2026-09-12 12:16:29 -0500

    Update test_arreglo.py

--- Hito 1/test_arreglo.py
+++ Hito 1/test_arreglo.py
@@ -1,16 +1,22 @@
 from arreglo_dinamico import ArregloDinamico
 from paciente import Paciente
 
-arreglo = ArregloDinamico()
-paciente1 = Paciente(1116182650, 20, 2, "08:30")
+arreglo = ArregloDinamico(2)
+paciente = Paciente(1116182650, 20, 2, "08:30")
 paciente2 = Paciente(1202566655, 25, 3, "04:50")
 paciente3 = Paciente(1505454555, 12, 2, "01:12")
 
-arreglo.insertar(paciente1)
+arreglo.insertar(paciente)
 arreglo.insertar(paciente2)
 arreglo.insertar(paciente3)
 
-print("La cantidad inicial es: ", arreglo.longitud())
+print("La capacidad del arreglo es: ", arreglo.capacidad)
+print("La cantidad de pacientes es: ", arreglo.longitud())
 print("El ID del primer paciente es: ", arreglo.obtener(0).id)
 print("El ID del segundo paciente es: ", arreglo.obtener(1).id)
 print("El ID del tercer paciente es: ", arreglo.obtener(2).id)
+
+arreglo.eliminar(1202566655)
+print("La cantidad después de eliminar es: ", arreglo.longitud())
+print("El ID del primer paciente es: ", arreglo.obtener(0).id)
+print("El ID del segundo paciente es: ", arreglo.obtener(1).id)
