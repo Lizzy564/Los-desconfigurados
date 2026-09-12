9e3f7efdca2e0e69bc50bbc10e8c3ec5caefe278
Lorenita_07 <medina.buitrago209@gmail.com>
2026-09-11 22:31:21 -0500

    Update paciente.py

--- Hito 1/paciente.py
+++ Hito 1/paciente.py
@@ -1,1 +1,6 @@
-
+class Paciente:
+    def __init__(self, id, edad, nivel_triage, hora_llegada):
+        self.id = id
+        self.edad = edad
+        self.nivel_triage = nivel_triage
+        self.hora_llegada = hora_llegada 
