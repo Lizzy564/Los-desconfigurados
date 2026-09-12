ad00f2cb91ee988b333d7825dfa7223f35fb0996
Lorenita_07 <medina.buitrago209@gmail.com>
2026-09-12 11:01:35 -0500

    Update test_arreglo.py

--- Hito 1/test_arreglo.py
+++ Hito 1/test_arreglo.py
@@ -2,8 +2,15 @@ from arreglo_dinamico import ArregloDinamico
 from paciente import Paciente
 
 arreglo = ArregloDinamico()
-paciente = Paciente(1116182650, 20, 2, "08:30")
-arreglo.insertar(paciente)
+paciente1 = Paciente(1116182650, 20, 2, "08:30")
+paciente2 = Paciente(1202566655, 25, 3, "04:50")
+paciente3 = Paciente(1505454555, 12, 2, "01:12")
+
+arreglo.insertar(paciente1)
+arreglo.insertar(paciente2)
+arreglo.insertar(paciente3)
 
 print("La cantidad inicial es: ", arreglo.longitud())
-print("El ID del paciente es: ", arreglo.obtener(0).id)
+print("El ID del primer paciente es: ", arreglo.obtener(0).id)
+print("El ID del segundo paciente es: ", arreglo.obtener(1).id)
+print("El ID del tercer paciente es: ", arreglo.obtener(2).id)
